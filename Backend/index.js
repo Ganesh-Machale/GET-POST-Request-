57805ca03fdd94ac0d0e13b84bbd3135fd8a036d
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-06-30 21:24:58 +0530

    Adding Middlewares for using app.post

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({extended:true}));
 
 const  port = 8080;
 app.get("/register",(req,res)=>{
-    res.send("Standard Get response");
+  let {user,password} = req.query;
+    res.send(`Standard Get response.Welcome ${user}`);
   });
   
   app.post("/register",(req,res)=>{
     res.send("Standard Post response");
   });
-  
+
   app.listen(port,()=>{
     console.log("listening to port at ",port);
   });
