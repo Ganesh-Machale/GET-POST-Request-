6836366e395ae8593d613fa240120a284252c875
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-06-30 21:26:37 +0530

    Add JSON body parsing middleware

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.urlencoded({extended:true}));
-
+app.use(express.json());
 const  port = 8080;
 app.get("/register",(req,res)=>{
   let {user,password} = req.query;
